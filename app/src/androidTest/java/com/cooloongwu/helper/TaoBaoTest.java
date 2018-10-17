c8dbed4d09e87d56f20c2ce103722efebfea76d3
CooLoongWu <742420210@qq.com>
2018-10-17 14:35:59 +0800

    使用自动化测试的方式实现淘宝直播自动点赞功能

---
+++ app/src/androidTest/java/com/cooloongwu/helper/TaoBaoTest.java
@@ -0,0 +1,44 @@
+package com.cooloongwu.helper;
+
+import android.app.Instrumentation;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by CooLoongWu on 2018-10-16 20:57.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class TaoBaoTest {
+
+    public Instrumentation instrumentation;
+    public UiDevice uiDevice;
+
+    @Before
+    public void setUp() {
+        instrumentation = InstrumentationRegistry.getInstrumentation();
+        uiDevice = UiDevice.getInstance(instrumentation);
+    }
+
+    //点赞按钮ID：com.taobao.taobao:id/taolive_favor_icon_config
+
+    @Test
+    public void test() throws InterruptedException {
+
+        //点击喜欢按钮
+        //按钮范围[618,1179][696,1257]
+        while (true) {
+            uiDevice.findObject(By.res(
+                    "com.taobao.taobao:id/taolive_favor_icon_config"))
+                    .click();
+            Thread.sleep(200);
+        }
+
+    }
+}
--- app/src/main/java/com/cooloongwu/helper/like/LikeFloatView.java
+++ app/src/main/java/com/cooloongwu/helper/like/LikeFloatView.java
@@ -36,36 +36,6 @@ public class LikeFloatView extends LinearLayout implements View.OnClickListener 
 
         buttonJump.setOnClickListener(this);
         buttonClose.setOnClickListener(this);
-
-//        text = new TextView(this.getContext());
-//        ((TextView) text).setText("两点间距离");
-//        ((TextView) text).setTextSize(18);
-//        text.setLayoutParams(new LinearLayout.LayoutParams(
-//                LayoutParams.WRAP_CONTENT,
-//                LayoutParams.WRAP_CONTENT));
-//        text.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-//
-//        dragView = new TextView(this.getContext());
-//        ((TextView) dragView).setText("拖动图1");
-//        ((TextView) dragView).setTextSize(18);
-//        dragView.setId(R.id.dragView1);
-//        dragView.setLayoutParams(new LinearLayout.LayoutParams(
-//                LayoutParams.WRAP_CONTENT,
-//                LayoutParams.WRAP_CONTENT));
-//        dragView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-//
-//        dragView2 = new TextView(this.getContext());
-//        ((TextView) dragView2).setText("拖动图2");
-//        ((TextView) dragView2).setTextSize(18);
-//        dragView2.setId(R.id.dragView2);
-//        dragView2.setLayoutParams(new LinearLayout.LayoutParams(
-//                LayoutParams.WRAP_CONTENT,
-//                LayoutParams.WRAP_CONTENT));
-//        dragView2.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-//
-//        this.addView(text);
-//        this.addView(dragView);
-//        this.addView(dragView2);
     }
 
 
