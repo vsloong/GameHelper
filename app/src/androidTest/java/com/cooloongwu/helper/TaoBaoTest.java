e0947ae04499f3513d16388c8899ec7a6efd383d
CooLoongWu <742420210@qq.com>
2018-10-19 10:00:42 +0800

    添加自动发言（刷评论）功能

--- app/src/androidTest/java/com/cooloongwu/helper/TaoBaoTest.java
+++ app/src/androidTest/java/com/cooloongwu/helper/TaoBaoTest.java
@@ -3,6 +3,7 @@ package com.cooloongwu.helper;
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
 
 import org.junit.Before;
@@ -25,20 +26,49 @@ public class TaoBaoTest {
         uiDevice = UiDevice.getInstance(instrumentation);
     }
 
-    //点赞按钮ID：com.taobao.taobao:id/taolive_favor_icon_config
+    //点赞按钮ID：com.taobao.taobao:id/taolive_favor_icon_config     [618,1179][696,1257]
+
+    //点击输入框：com.taobao.taobao:id/taolive_chat_btn_text     [141,1179][408,1257]
+    //输入框ID：com.taobao.taobao:id/taolive_edit_text
+    //发送按钮ID：com.taobao.taobao:id/taolive_edit_send    [600,646][720,726]
 
     @Test
     public void test() throws InterruptedException {
-        //点击喜欢按钮
+//        autoInput();
+        autoLike();
+    }
+
+    /**
+     * 自动发言功能
+     *
+     * @throws InterruptedException 异常
+     */
+    private void autoInput() throws InterruptedException {
+        while (true) {
+            uiDevice.click(400, 1200);
+            Thread.sleep(500);
+            //这里需要优化，淘宝会提示操作过于频繁
+            uiDevice.findObject(By.res("com.taobao.taobao:id/taolive_edit_text"))
+                    .setText("非常棒哦！");
+            Thread.sleep(200);
+            uiDevice.click(700, 700);
+            Thread.sleep(300);
+        }
+    }
+
+    /**
+     * 自动点赞功能
+     *
+     * @throws InterruptedException 异常
+     */
+    private void autoLike() throws InterruptedException {
         //按钮范围[618,1179][696,1257]
         while (true) {
             uiDevice.click(620, 1200);
-            Thread.sleep(100);
-            uiDevice.click(622, 1202);
+//            uiDevice.click(622, 1202);
 //            uiDevice.findObject(By.res(
 //                    "com.taobao.taobao:id/taolive_favor_icon_config"))
 //                    .click();
         }
-
     }
 }
